48303f74ddc4b094461271212bea58f913f3aee8
jacquelynelias <jacquelynrelias@gmail.com>
2023-07-24 23:26:07 -0400

    updates to projects

--- assets/work.js
+++ assets/work.js
@@ -1,6 +1,14 @@
 var samples = { 
 	"pieces": [
 	{
+			"date": "2023-06-30",
+			"title": "The Supreme Court’s Admissions Ruling Mainly Affects Selective Colleges. They’re a Tiny Slice of Higher Ed.",
+			"cta":'<a href="https://www.chronicle.com/article/the-supreme-courts-admissions-ruling-mainly-affects-selective-colleges-theyre-a-tiny-slice-of-higher-ed" target="_blank"></a>',
+			"promo":"./img/2023-selectivity/promo.png",
+			"category": [2,3]
+	
+	},	
+	{
 		"date": "2016-03-29",
 		"title": "Who Does Your College Think Its Peers Are?",
 		"cta":'<a href="https://www.chronicle.com/article/who-holds-americas-1-5-trillion-student-loan-debt/" target="_blank"></a>',
@@ -9,19 +17,55 @@ var samples = {
 
 	},	
 	{
+		"date": "2020-05-22",
+		"title": "DEI Legislation Tracker",
+		"cta":'<a href="https://www.chronicle.com/article/welcome-to-the-socially-distanced-campus" target="_blank"></a>',
+		"promo":"./img/2023-dei-tracker/promo.webp",
+		"category": [2,4]
+	},
+	{
+		"date": "2022-11-16",
+		"title": "Test-Optional Policies Now Dominate Higher Ed",
+		"cta":'<a href="Test-Optional Policies Now Dominate Higher Ed" target="_blank"></a>',
+		"promo":"./img/2021-books/promo.png",
+		"category": [2,3]
+	},
+	{
+		"date": "2021-07-21",
+		"title": "These Are the Books That Colleges Think Every Freshman Should Read",
+		"cta":'<a href="https://www.chronicle.com/article/these-are-the-books-that-colleges-think-every-freshman-should-read" target="_blank"></a>',
+		"promo":"./img/2021-books/promo.png",
+		"category": [1,2,3]
+	},
+	{
+		"date": "2020-05-22",
+		"title": "Welcome to the socially distanced campus?",
+		"cta":'<a href="https://www.chronicle.com/article/welcome-to-the-socially-distanced-campus" target="_blank"></a>',
+		"promo":"./img/2020-animation/dorm.jpeg",
+		"category": [4]
+	},
+	{
 		"date": "2016-08-05",
-		"title": "Who holds America's 1.5",
+		"title": "Who holds America's $1.5 trillion student-loan debt",
 		"cta":'<a href="https://www.chronicle.com/article/who-holds-americas-1-5-trillion-student-loan-debt/" target="_blank"></a>',
 		"promo":"./img/2020_student_debt_full/promo.png",
 		"category": [1, 2, 3]
 
 	},
 	{
-		"date": "2016-03-29",
+		"date": "2020-01-11",
 		"title": "When the candidates come to campus",
 		"cta":'<a href="https://www.chronicle.com/article/when-the-candidates-come-to-campus/" target="_blank"></a>',
 		"promo":"./img/2020-candidates/promo.png",
-		"category": [1,2,4]
+		"category": [1,2,3]
+
+	},
+	{
+		"date": "2019-04-04",
+		"title": "How America’s College-Closure Crisis Leaves Families Devastated",
+		"cta":'<a href="https://www.chronicle.com/article/how-americas-college-closure-crisis-leaves-families-devastated/" target="_blank"></a>',
+		"promo":"./img/2019-closures/promo.png",
+		"category": [4]
 
 	},
 	
